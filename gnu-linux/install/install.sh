92a7a36aab624d1e1d78d2127f7ada6ef92e4739
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2026-02-17 11:22:51 +0800

    update install.sh
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- gnu-linux/install/install.sh
+++ gnu-linux/install/install.sh
@@ -132,7 +132,7 @@ install_code_server()
 fedora_docker()
 {
 	sudo dnf group install development-tools -y
-	sudo dnf -y install ncurses-devel clang llvm flex bison bc kmod pahole lld ccache openssl-devel openssl
+	sudo dnf -y install ncurses-devel clang llvm flex bison bc kmod pahole lld ccache openssl-devel openssl git-email
 	sudo dnf -y install bridge-utils iptables dnsmasq net-tools
 	sudo dnf -y install vim emacs global tmux wget ps ping
 	sudo dnf install @virtualization -y
