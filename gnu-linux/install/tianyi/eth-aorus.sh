d89d1e6081eced56f6387f6aeb4dca858a030c04
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2026-06-02 22:26:02 +0800

    update install/
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- gnu-linux/install/tianyi/eth-aorus.sh
+++ gnu-linux/install/tianyi/eth-aorus.sh
@@ -1,7 +1,8 @@
 ssh_cmd="ssh -t chenxiaosong@172.21.20.210"
 
 if [ $# -ge 1 ]; then
-	$ssh_cmd "docker exec -it workspace-fedora bash -c 'tmux att -t $1; exec bash'; exec bash -l"
+	# $ssh_cmd "docker exec -it workspace-fedora bash -c 'export LC_ALL=C.UTF-8; tmux att -t $1; exec bash'; exec bash -l" # 退出tmux不会立刻回到原来的环境
+	$ssh_cmd "docker exec -it workspace-fedora bash -c 'export LC_ALL=C.UTF-8; tmux att -t $1'"
 else
 	$ssh_cmd
 fi
